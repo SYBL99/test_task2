a854dde441218b32bcc1b2911266a879eb5b2f24
Дмитрий <dmitriy.shupikov@gmail.com>
2022-07-26 20:37:01 +0700

    about location

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
-import GetInfo from "./API/PostService";
-import Episode from "./components/episode/Episode.jsx";
-import EpisodeList from "./components/episodesList/EpisodeList.jsx";
 import "./css/App.css"
 import Layout from "./components/pages/Layout.jsx";
 import {Routes, Route} from "react-router-dom";
 import Home from "./components/pages/Home.jsx"
 import AboutEpisode from "./components/pages/AboutEpisode.jsx"
 import NotFound from "./components/pages/NotFound.jsx";
+import Locations from "./components/pages/Locations.jsx";
+import AboutLocation from "./components/pages/AboutLocation.jsx";
 
 function App () {
     return (
@@ -16,6 +15,8 @@ function App () {
                 <Route path='/' element={<Layout/>}>
                     <Route index element={<Home/>}/>
                     <Route path="/about/:id" element={<AboutEpisode/>}/>
+                    <Route path="/locations" element={<Locations />}/>
+                    <Route path="/locations/:id" element={<AboutLocation />} />
                     <Route path="*" element={<NotFound />}/>
                 </Route>
             </Routes>
